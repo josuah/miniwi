43a8f6606f2013164653ec06ce444799a6f3f89b
sshbio <jd@ssh.bio>
2016-06-18 14:11:04 +0200

    Updated generate script

--- bdf-to-psf/generate-miniwi.sh
+++ bdf-to-psf/generate-miniwi.sh
@@ -1,1 +1,3 @@
-./bdf-to-psf --fb ../miniwi.bdf equivalents uni-2.set+box-blocks.set 512 ../miniwi.psf
+#!/bin/sh
+./bdf-to-psf-8 --fb ../miniwi-8.bdf equivalents uni-2.set+box-blocks.set 512 ../miniwi-8.psf
+./bdf2psf --fb ../miniwi-16.bdf equivalents uni-2.set+box-blocks.set 512 ../miniwi-16.psf
